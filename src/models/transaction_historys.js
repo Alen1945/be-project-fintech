21c1ac057bd95afbfa4f43ae29a0f69d32ecd97c
Alen1945 <dwialendraipangselly23@gmail.com>
2020-04-04 04:01:41 +0700

    add message to model history transaction

--- src/models/transaction_historys.js
+++ src/models/transaction_historys.js
@@ -1,16 +1,30 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const transaction_historys = sequelize.define('transaction_historys', {
-    id_type_trasaction: DataTypes.INTEGER,
-    id_sender: DataTypes.INTEGER,
-    id_receiver: DataTypes.INTEGER,
-    amount: DataTypes.DECIMAL,
-    isDelete: DataTypes.TINYINT
-  }, {});
+  const transaction_historys = sequelize.define(
+    "transaction_historys",
+    {
+      id_type_trasaction: DataTypes.INTEGER,
+      id_sender: DataTypes.INTEGER,
+      id_receiver: DataTypes.INTEGER,
+      amount: DataTypes.DECIMAL,
+      message: DataTypes.TEXT,
+      isDelete: DataTypes.TINYINT,
+    },
+    {}
+  );
   transaction_historys.associate = function (models) {
-    transaction_historys.belongsTo(models.type_transactions, { foreignKey: 'id_type_trasaction', targetKey: 'id' })
-    transaction_historys.belongsTo(models.users, { foreignKey: 'id_sender', targetKey: 'id' })
-    transaction_historys.belongsTo(models.users, { foreignKey: 'id_receiver', targetKey: 'id' })
+    transaction_historys.belongsTo(models.type_transactions, {
+      foreignKey: "id_type_trasaction",
+      targetKey: "id",
+    });
+    transaction_historys.belongsTo(models.users, {
+      foreignKey: "id_sender",
+      targetKey: "id",
+    });
+    transaction_historys.belongsTo(models.users, {
+      foreignKey: "id_receiver",
+      targetKey: "id",
+    });
   };
   return transaction_historys;
 };
